ae7056469c82a0f9d035af4afe203ca85c4d2b1f
Dylan Bulmer <dylan@bulmersolutions.com>
2021-02-21 17:48:52 -0500

    update db solution

--- pages/api/user.js
+++
@@ -1,8 +0,0 @@
-import nextConnect from 'next-connect';
-import middleware from '@/middleware';
-
-const handler = nextConnect();
-handler.use(middleware);
-handler.get(async (req, res) => res.json({ user: req.user }));
-
-export default handler;
