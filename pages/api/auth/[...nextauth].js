bc99695eaef0b0f54c8778046a0f7716a7514e16
Dylan Bulmer <dylan@bulmersolutions.com>
2021-03-29 12:28:55 -0400

    implement react-native app signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,32 +9,75 @@ import Models from "../../../models/typeorm";
 
 connect();
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      params: {
-        grant_type: "authorization_code",
-      },
-    }),
-  ],
-  adapter: Adapters.TypeORM.Adapter(
-    // The first argument should be a database connection string or TypeORM config object
-    process.env.MONGODB_URL,
-    // The second argument can be used to pass custom models and schemas
-    {
-      models: {
-        User: Models.User,
-      },
-    }
-  ),
-  secret: process.env.SECRET,
-  session: {
-    jwt: true,
-  },
-  pages: {
-    error: '/login'
-  },
-});
+function reverseDomain(domain) {
+  return domain.split(".").reverse().join(".");
+}
+
+const getProviders = (ownership, platform, codeVerifier) => {
+  return [
+    ownership === "expo"
+      ? Providers.Google({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          params: {
+            grant_type: "authorization_code",
+            redirect_uri: "https://auth.expo.io/@dylanbulmer/annotator",
+          },
+        })
+      : platform === "ios"
+      ? Providers.Google({
+          clientId: process.env.GOOGLE_IOS_CLIENT_ID,
+          params: {
+            grant_type: "authorization_code",
+            redirect_uri: `${reverseDomain(
+              process.env.GOOGLE_IOS_CLIENT_ID
+            )}:/oauthredirect`,
+            code_verifier: codeVerifier,
+          },
+        })
+      : platform === "android"
+      ? Providers.Google({
+          clientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
+          params: {
+            grant_type: "authorization_code",
+            redirect_uri: "com.bulmersolutions.annotator:/oauthredirect",
+            code_verifier: codeVerifier,
+          },
+        })
+      : Providers.Google({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          params: {
+            grant_type: "authorization_code",
+          },
+        }),
+  ];
+};
+
+export default function handler(req, res) {
+  return NextAuth(req, res, {
+    // Configure one or more authentication providers
+    providers: getProviders(
+      req.headers["annotator-ownership"],
+      req.headers["annotator-platform"],
+      req.headers["annotator-code-verifier"]
+    ),
+    adapter: Adapters.TypeORM.Adapter(
+      // The first argument should be a database connection string or TypeORM config object
+      process.env.MONGODB_URL,
+      // The second argument can be used to pass custom models and schemas
+      {
+        models: {
+          User: Models.User,
+        },
+      }
+    ),
+    secret: process.env.SECRET,
+    session: {
+      jwt: true,
+    },
+    pages: {
+      error: "/login",
+    },
+  });
+}
